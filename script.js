2e96432013a06acb50eb1b0b49a4721529bd7d57
ENG AYUUB <moztaphem9@gmal.com>
2023-06-25 23:41:45 +0300

    Vowels-app

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const inputText = document.getElementById("input-text");
+const countButton = document.getElementById("count-btn");
+const vowelCount = document.getElementById("vowel-count");
 
+function countVowels() {
+  const vowelss  = inputText.value.toLowerCase();
+  const vowels = ["a", "e", "i", "o", "u"];
+  let count = 0;
+
+  for (let i = 0; i < vowelss.length; i++) {
+    if (vowels.includes(vowelss [i])) {
+      count++;
+    }
+  }
+
+  vowelCount.textContent = count;
+}
+countButton.addEventListener("click", countVowels);
